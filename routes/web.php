810cbcb33cbc0ee36ed362e32e976e9a34af2f19
abdelrahmanYouseff <abdelrahman.yousef@hadaf-hq.com>
2025-07-27 10:31:38 +0300

    edit qr

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,29 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Route to display PDF files
+Route::get('qrs/{id}', function ($id) {
+    $pdfFiles = [
+        1 => 'Delawa Menu.pdf',
+        2 => 'منيو بكيزة.pdf'
+    ];
+
+    if (!isset($pdfFiles[$id])) {
+        abort(404, 'QR code not found');
+    }
+
+    $fileName = $pdfFiles[$id];
+    $pdfPath = public_path('pdf/' . $fileName);
+
+    if (file_exists($pdfPath)) {
+        return response()->file($pdfPath, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="' . $fileName . '"'
+        ]);
+    }
+
+    abort(404, 'PDF file not found');
+})->name('qrs.show');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
